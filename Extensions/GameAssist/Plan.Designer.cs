25be352b31d7924e4800a52cfec5dec40c2adde6
nnoldman <1527053436@qq.com>
2016-07-06 19:44:06 +0800

    ADD:GameAssist

---
+++ Extensions/GameAssist/Plan.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace GameAssist
+{
+    public partial class Plan
+    {
+        /// <summary> 
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region 组件设计器生成的代码
+
+        /// <summary> 
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.fileTree = new System.Windows.Forms.TreeView();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.openInExplorerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // fileTree
+            // 
+            this.fileTree.ContextMenuStrip = this.contextMenuStrip1;
+            this.fileTree.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.fileTree.Font = new System.Drawing.Font("宋体", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.fileTree.LabelEdit = true;
+            this.fileTree.Location = new System.Drawing.Point(0, 0);
+            this.fileTree.Name = "fileTree";
+            this.fileTree.Size = new System.Drawing.Size(403, 486);
+            this.fileTree.TabIndex = 0;
+            this.fileTree.NodeMouseClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.fileTree_NodeMouseClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openInExplorerToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(169, 48);
+            // 
+            // openInExplorerToolStripMenuItem
+            // 
+            this.openInExplorerToolStripMenuItem.Name = "openInExplorerToolStripMenuItem";
+            this.openInExplorerToolStripMenuItem.Size = new System.Drawing.Size(168, 22);
+            this.openInExplorerToolStripMenuItem.Text = "OpenInExplorer";
+            this.openInExplorerToolStripMenuItem.Click += new System.EventHandler(this.openInExplorerToolStripMenuItem_Click);
+            // 
+            // SolutionExplorer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.ClientSize = new System.Drawing.Size(403, 486);
+            this.Controls.Add(this.fileTree);
+            this.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Name = "SolutionExplorer";
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView fileTree;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem openInExplorerToolStripMenuItem;
+    }
+}
---
+++ Extensions/GameAssist/Plan.cs
@@ -0,0 +1,180 @@
+﻿using Core;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WeifenLuo.WinFormsUI.Docking;
+
+namespace GameAssist
+{
+    [Core.ExtensionVersion(Name = "Plan")]
+    public partial class Plan : Extension, ATrigger.IStaticEmitterContainer
+    {
+        static Plan Instance;
+
+        int mIndexDir = 0;
+        int mIndexDirSel = 1;
+        //int mIndexFile;
+
+        public Plan()
+        {
+            Instance = this;
+            InitializeComponent();
+            ATrigger.DataCenter.AddInstance(this);
+            this.fileTree.ImageList = new ImageList();
+            this.fileTree.NodeMouseDoubleClick += fileTree_NodeMouseDoubleClick;
+            this.fileTree.KeyUp += fileTree_KeyUp;
+            this.fileTree.ShowNodeToolTips = true;
+            this.fileTree.ShowLines = false;
+            this.fileTree.ShowPlusMinus = false;
+        }
+
+        void fileTree_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyValue == (int)Keys.F2)
+            {
+                TreeNode node = sender as TreeNode;
+                if (node != null)
+                    node.BeginEdit();
+            }
+        }
+
+        void fileTree_NodeMouseDoubleClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+            if (Center.DocumentManager.Documents.Contains(e.Node.Tag.ToString()))
+            {
+                Center.ActiveDocument.value = e.Node.Tag.ToString();
+            }
+            else if (File.Exists(e.Node.Tag.ToString()))
+            {
+                Center.CurrentOpenDoucment.Set(e.Node.Tag.ToString(),true,true);
+            }
+        }
+
+        protected override void OnFormClosed(System.Windows.Forms.FormClosedEventArgs e)
+        {
+            ATrigger.DataCenter.RemoveInstance(this);
+            base.OnFormClosed(e);
+            Instance = null;
+        }
+
+        [AddMenu("File(&F)/Select Resource Floder")]
+        static void SelectFloder()
+        {
+            if (Instance == null)
+                CreateInstance();
+
+            FolderBrowserDialog dlg = new FolderBrowserDialog();
+            dlg.SelectedPath = Center.Option.Solution.LastSolutionPath;
+            var result = dlg.ShowDialog();
+
+            if (result == System.Windows.Forms.DialogResult.OK)
+                Center.OnOpenFloder.value = dlg.SelectedPath;
+        }
+
+        static void CreateInstance()
+        {
+            Instance = new Plan();
+            Instance.TabText = "Plan";
+            Instance.Show(Center.Form.DockerContainer, DockState.Float);
+        }
+
+        [AddMenu("View(&V)/Plan")]
+        static void OnOpenView()
+        {
+            if (Instance == null)
+            {
+                CreateInstance();
+            }
+            else
+            {
+                Instance.Hide();
+                Instance.Dispose();
+                Instance = null;
+            }
+        }
+
+        [ATrigger.Receiver((int)DataType.OpenFloder)]
+        void OnOpenFloder()
+        {
+            string floderName = Center.OnOpenFloder.value;
+
+            if (!Directory.Exists(floderName))
+                return;
+            Center.Option.Solution.LastSolutionPath = floderName;
+            DoOpen();
+        }
+
+        void DoOpen()
+        {
+            if (string.IsNullOrEmpty(Center.Option.Solution.LastSolutionPath))
+                return;
+
+            this.fileTree.Nodes.Clear();
+
+            TreeNode root = new TreeNode(Path.GetFileName(Center.Option.Solution.LastSolutionPath));
+            root.Tag = Center.Option.Solution.LastSolutionPath;
+            this.fileTree.Nodes.Add(root);
+            AddFiles(root, Center.Option.Solution.LastSolutionPath);
+        }
+
+        void AddFiles(TreeNode parent, string floderName)
+        {
+            var dirInfo = new DirectoryInfo(floderName);
+
+            var files = dirInfo.GetFiles();
+
+            var dirs = dirInfo.GetDirectories();
+
+            foreach (var dir in dirs)
+            {
+                var child = new TreeNode(dir.Name);
+                child.Tag = dir.FullName;
+                child.ToolTipText = dir.FullName;
+                child.ImageIndex = mIndexDir;
+                parent.Nodes.Add(child);
+                AddFiles(child, dir.FullName);
+            }
+
+            foreach (var f in files)
+            {
+                if (Center.Option.Solution.IgnoreExtensions.Contains(f.Extension))
+                    continue;
+
+                var child = new TreeNode(f.Name);
+                child.ImageIndex = mIndexDirSel;
+                child.SelectedImageIndex = mIndexDirSel;
+                parent.Nodes.Add(child);
+                child.Tag = f.FullName;
+                child.ToolTipText = f.FullName;
+            }
+        }
+        protected override void OnShown(EventArgs e)
+        {
+            base.OnShown(e);
+            DoOpen();
+        }
+
+        private void fileTree_NodeMouseClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                this.fileTree.SelectedNode = e.Node;
+                Point pos = new Point(e.Node.Bounds.X + e.Node.Bounds.Width, e.Node.Bounds.Y + e.Node.Bounds.Height / 2);
+                this.contextMenuStrip1.Show(e.Node.TreeView, pos);
+            }
+        }
+
+        private void openInExplorerToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string path = this.fileTree.SelectedNode.Tag.ToString();
+
+            Shell.OpenFloder(path);
+        }
+
+    }
+}
