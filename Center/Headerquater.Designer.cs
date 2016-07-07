3f858b5c6831d203ee7dd935f746fc80ba978e9d
nnoldman <1527053436@qq.com>
2016-07-07 15:43:19 +0800

    1

--- Center/Headerquater.Designer.cs
+++ Center/Headerquater.Designer.cs
@@ -28,11 +28,54 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 320);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel2.Location = new System.Drawing.Point(474, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(200, 320);
+            this.panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(200, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(274, 320);
+            this.panel3.TabIndex = 2;
+            // 
+            // Headerquater
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(674, 320);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Name = "Headerquater";
             this.Text = "Headerquater";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
     }
 }
--- Center/Headerquater.cs
+++ Center/Headerquater.cs
@@ -7,14 +7,46 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WeifenLuo.WinFormsUI.Docking;
 
 namespace Core
 {
-    public partial class Headerquater : Form
+    public partial class Headerquater : Extension
     {
+        static Headerquater Instance;
+
         public Headerquater()
         {
+            Instance = this;
             InitializeComponent();
+        }
+
+        [AddMenu("Headerquater(&H)")]
+        static void OnOpenView()
+        {
+            if (Instance == null)
+            {
+                CreateInstance();
+            }
+            else
+            {
+                Instance.Hide();
+                Instance.Dispose();
+                Instance = null;
+            }
+        }
+
+        static void CreateInstance()
+        {
+            Instance = new Headerquater();
+            Instance.TabText = "Headerquater";
+            Instance.Show(Center.Form.DockerContainer, DockState.Float);
+        }
+
+        protected override void OnFormClosed(System.Windows.Forms.FormClosedEventArgs e)
+        {
+            base.OnFormClosed(e);
+            Instance = null;
         }
     }
 }
